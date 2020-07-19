3c7b51319d17e75772e7fda29432c2f7f954500a
chaitali Barhate <chaitali.barhate@adapty.com>
2020-07-19 23:20:25 +0530

    Added build folder

---
+++ react-assignment/build/precache-manifest.935b324171a45a9696c87b539d21c23f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f1c8fb2c990849af6e592849d543fcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6217918afa8689432d28",
+    "url": "/static/css/main.cd44ba6a.chunk.css"
+  },
+  {
+    "revision": "c895399312d523499a8c",
+    "url": "/static/js/2.a07d1464.chunk.js"
+  },
+  {
+    "revision": "99c1bf48081ed736382f4cc79d8323d8",
+    "url": "/static/js/2.a07d1464.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6217918afa8689432d28",
+    "url": "/static/js/main.29c47381.chunk.js"
+  },
+  {
+    "revision": "7a389cb181c09669c0a9",
+    "url": "/static/js/runtime-main.5744deea.js"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  }
+]);
